45ff6f46dc5e31651d75691a7ed3c355e69ad18e
Crispy Strawberry <crispy-strawberry102938@outlook.com>
2023-11-14 17:28:11 +0530

    Move from `ArrayList` to `ArrayListUnmanaged`.
    Move the underlying data structure of the String from
    ArrayList to ArrayListUnmanaged. This is beneficial
    because previously, std.mem.Allocator was being stored
    two times, once in the allocator field of String and
    then inside ArrayList. Moving from ArrayList to
    ArrayListUnmanaged changed the size from 56 bytes to
    40 bytes.

--- src/main.zig
+++ src/main.zig
@@ -3,28 +3,37 @@ const testing = std.testing;
 
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const ArrayListUnmanaged = std.ArrayListUnmanaged;
 
 /// A heap allocated utf-8 encoded mutable
 /// string type.
 /// Inspired by Rust's `String`.
 pub const String = struct {
     allocator: std.mem.Allocator,
-    buf: ArrayList(u8),
+    buf: ArrayListUnmanaged(u8),
 
     /// Creates a new `String` of zero length.
     /// This is guaranteed to not do any allocations.
     pub fn init(allocator: std.mem.Allocator) String {
         return String{
             .allocator = allocator,
-            .buf = ArrayList(u8).init(allocator),
+            .buf = ArrayListUnmanaged(u8),
         };
     }
 
     pub fn initCapacity(allocator: Allocator, cap: usize) Allocator.Error!String {
         return String{
             .allocator = allocator,
-            .buf = try ArrayList(u8).initCapacity(allocator, cap),
+            .buf = try ArrayListUnmanaged(u8).initCapacity(allocator, cap),
         };
+    }
+
+    /// A helper function for string literals.
+    /// WARNING: This function should only be used with string literals
+    /// like `"Hello World"` etc which are guaranteed to be valid utf-8.
+    /// For general byte arrays, see `fromUtf8` or `fromUtf8Unchecked` instead.
+    pub fn fromStr(allocator: Allocator, str_literal: []const u8) Allocator.Error!String {
+        return fromUtf8Unchecked(allocator, str_literal);
     }
 
     /// Creates a new `String` from the given buffer without checking if the
@@ -44,7 +53,7 @@ pub const String = struct {
     /// The `String` takes ownership of the `ArrayList`. Memory should be freed with
     /// `deinit`
     pub fn fromUtf8ArrayListUnchecked(buf: ArrayList(u8)) String {
-        return String{ .allocator = buf.allocator, .buf = buf };
+        return String{ .allocator = buf.allocator, .buf = buf.moveToUnmanaged() };
     }
 
     /// Returns length of string in bytes
@@ -60,18 +69,22 @@ pub const String = struct {
         return self.buf.items;
     }
 
+    /// Returns a pointer to the contents of the String.
+    /// Pointers are not guaranteed to remain stable between mutations
     pub fn asPtr(self: *const String) [*]u8 {
         return self.buf.items.ptr;
     }
 
-    pub fn intoArrayList(self: *String) ArrayList(u8) {
-        const arr = self.buf;
-        self.buf = ArrayList(u8).init(self.allocator);
+    /// Returns the underlying `ArrayList`. It is the caller's responsibility
+    /// to free the `ArrayList`.
+    pub fn intoArrayList(self: *String) ArrayListUnmanaged(u8) {
+        const arr = self.buf.toManaged(self.allocator);
+        self.buf = ArrayListUnmanaged(u8);
         return arr;
     }
 
     pub fn intoBytes(self: *String) ![]u8 {
-        return try self.buf.toOwnedSlice();
+        return try self.buf.toOwnedSlice(self.allocator);
     }
 
     /// Check if all characters are within ascii range.
@@ -90,25 +103,26 @@ pub const String = struct {
     }
 
     pub fn deinit(self: *const String) void {
-        self.buf.deinit();
+        ArrayListUnmanaged(u8).deinit(@constCast(&self.buf), self.allocator);
+        @constCast(self).* = undefined;
     }
 };
 
 const alloc = testing.allocator;
 test "test is_ascii" {
-    const ascii_str = try String.fromUtf8Unchecked(alloc, "Hello World");
+    const ascii_str = try String.fromStr(alloc, "Hello World");
     defer ascii_str.deinit();
 
     std.debug.print("{}\n", .{ascii_str.capacity()});
     try testing.expectEqual(true, ascii_str.isAscii());
 
-    const emoji_str = try String.fromUtf8Unchecked(alloc, "Hello ༼ つ ◕_◕ ༽つ, 😀");
+    const emoji_str = try String.fromStr(alloc, "Hello ༼ つ ◕_◕ ༽つ, 😀");
     defer emoji_str.deinit();
 
     try testing.expectEqual(false, emoji_str.isAscii());
 
     var byte_str = [_]u8{ 67, 68, 69, 70 };
-    const my_str = try String.fromUtf8Unchecked(alloc, byte_str[0..]);
+    const my_str = try String.fromStr(alloc, byte_str[0..]);
     defer my_str.deinit();
 
     std.debug.print("{s}\n", .{my_str.asBytes()});
