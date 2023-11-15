c6d28e7fc35ad59587791d4adcee11c2eb8f146c
Crispy Strawberry <crispy-strawberry102938@outlook.com>
2023-11-16 01:01:02 +0530

    Add more functions and concrete error types.
    Concrete error sets allow for a more streamlined
    experience for the end user as they can see exactly
    what kind of errors can occur during a function.
    Also add some functions like clone, fromOwnedSlice etc

--- src/string.zig
+++ src/string.zig
@@ -5,9 +5,15 @@ const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const ArrayListUnmanaged = std.ArrayListUnmanaged;
 
-/// A heap allocated utf-8 encoded mutable
-/// string type.
+pub const StringError = error{
+    OutOfMemory,
+    Utf8ValidationError,
+};
+
+/// An allocated UTF-8 encoded mutable string type.
 /// Inspired by Rust's `String`.
+/// Provides various functions that make working
+/// with strings easier.
 pub const String = @This();
 
 allocator: std.mem.Allocator,
@@ -26,6 +32,13 @@ pub fn initCapacity(allocator: Allocator, cap: usize) Allocator.Error!String {
     return String{
         .allocator = allocator,
         .buf = try ArrayListUnmanaged(u8).initCapacity(allocator, cap),
+    };
+}
+
+pub fn clone(self: String) Allocator.Error!String {
+    return String{
+        .allocator = self.allocator,
+        .buf = try self.buf.clone(self.allocator),
     };
 }
 
@@ -61,9 +74,31 @@ pub fn fromUtf8ArrayListUnchecked(buf: ArrayList(u8)) String {
 /// This function validates the string contents before writing them.
 /// In case the string does not contain valid UTF-8, it returns an error.
 /// TODO: Validate the slice using own implemenation.
-pub fn fromUtf8(allocator: Allocator, buf: []const u8) !String {
+pub fn fromUtf8(allocator: Allocator, buf: []const u8) StringError!String {
     if (std.unicode.utf8ValidateSlice(buf)) {
         return String.fromUtf8Unchecked(allocator, buf);
+    } else {
+        return error.Utf8ValidationError;
+    }
+}
+
+pub fn fromOwnedSlice(allocator: Allocator, buf: []u8) error.Utf8ValidationError!String {
+    if (!std.unicode.utf8ValidateSlice(buf)) {
+        return error.Utf8ValidationError;
+    }
+
+    return String{
+        .allocator = allocator,
+        .buf = ArrayListUnmanaged(u8).fromOwnedSlice(buf),
+    };
+}
+
+/// Converts an `ArrayList` to a `String` but checks if the string
+/// is valid UTF-8. If you don't know whether your `ArrayList` is
+/// valid UTF-8 or not, then you should probably use this function.
+pub fn fromUtf8ArrayList(buf: ArrayList(u8)) StringError!String {
+    if (std.unicode.utf8ValidateSlice(buf.items)) {
+        return String.fromUtf8ArrayListUnchecked(buf);
     } else {
         return error.Utf8ValidationError;
     }
@@ -82,6 +117,10 @@ pub fn toSlice(self: *const String) []u8 {
     return self.buf.items;
 }
 
+pub fn toOwnedSlice(self: *String) Allocator.Error![]u8 {
+    return self.buf.toOwnedSlice(self.allocator);
+}
+
 /// Returns a pointer to the contents of the String.
 /// Pointers are not guaranteed to remain stable between mutations
 pub fn asPtr(self: *const String) [*]u8 {
@@ -90,13 +129,13 @@ pub fn asPtr(self: *const String) [*]u8 {
 
 /// Returns the underlying `ArrayList`. It is the caller's responsibility
 /// to free the `ArrayList`.
-pub fn intoArrayList(self: *String) ArrayListUnmanaged(u8) {
+pub fn toArrayList(self: *String) ArrayListUnmanaged(u8) {
     const arr = self.buf.toManaged(self.allocator);
     self.buf = ArrayListUnmanaged(u8);
     return arr;
 }
 
-pub fn intoBytes(self: *String) ![]u8 {
+pub fn intoBytes(self: *String) Allocator.Error![]u8 {
     return self.buf.toOwnedSlice(self.allocator);
 }
 
@@ -109,7 +148,7 @@ pub fn appendStr(self: *String, str_literal: []const u8) Allocator.Error!void {
 }
 
 /// Check if all characters are within ascii range.
-/// TODO: Optimize using vectors
+/// TODO: Replace this implementation with vectorized implemention.
 pub fn isAscii(self: *const String) bool {
     // const vec1 = @Vector(4, u8) { 123, 34, 65, 32 };
     // const vec2: @Vector(4, u8) = @splat(127);
@@ -187,7 +226,6 @@ test "isAscii vs isAsciiVectorized" {
     defer str.deinit();
 
     var timer = try std.time.Timer.start();
-    timer.reset();
     const scalar_ascii = str.isAscii();
     const t2 = timer.lap();
     const vector_ascii = str.isAsciiVectorized();
