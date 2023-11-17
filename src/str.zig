63cfb9a3ab81a221a4fcc65a65967c899ff8d79a
Crispy Strawberry <crispy-strawberry102938@outlook.com>
2023-11-17 10:40:57 +0530

    Change of plans, i am providing functions for directly working with slices

--- src/str.zig
+++ src/str.zig
@@ -1,10 +1,26 @@
 const std = @import("std");
 
-/// An immutable reference to a unicode encoded buffer.
-/// `Str` does not own the buffer but provided
-/// functions that make working with the buffer
-/// easier.
-/// Inspired by Rust's `&str`
-const Str = @This();
+// /// An immutable reference to a unicode encoded buffer.
+// /// `Str` does not own the buffer but provided
+// /// functions that make working with the buffer
+// /// easier.
+// /// Inspired by Rust's `&str`
+// const Str = @This();
 
-buf: []const u8,
+// buf: []const u8,
+
+// Wait, I have changed my mind, this file will provide functions
+// for directly working with slices, and not necessarily immutable.
+
+pub fn utf8ValidateSlice(buf: []const u8) bool {
+    return std.unicode.utf8ValidateSlice(buf);
+}
+
+pub fn isAscii(buf: []const u8) bool {
+    for (buf) |char| {
+        if (char >= 128) {
+            return false;
+        }
+    }
+    return true;
+}
